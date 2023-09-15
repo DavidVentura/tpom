b5d18fdd7185ebf32a9f2b2cf685ed103290967a
DavidVentura <davidventura27@gmail.com>
2023-09-15 21:28:38 +0000

    keep improving api and fix tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # TPOM
 //! Allows replacing time-related functions in the vDSO ([1](https://man7.org/linux/man-pages/man7/vdso.7.html), [2](https://en.wikipedia.org/wiki/VDSO)) with user-provided functions.  
 //!
-//! Only works on Linux. Is currently limited to x86_64, aarch64 and riscv, though it could be extended for other architectures.
+//! Only works on Linux. Is currently limited to x86_64, AArch64 and RISC-V, though it could be extended for other architectures.
 //!
 //! Replaces these functions, if provided:
 //!
@@ -17,32 +17,34 @@
 //! use tpom::*;
 //! use std::time::SystemTime;
 //!
-//! ClockController::overwrite(
-//!     Some(|_| TimeSpec {
-//!         seconds: 1,
-//!         nanos: 1,
-//!     }),
-//!     None,
-//!     None,
-//!     None,
-//! );
+//! fn myclock(_clockid: i32) -> TimeSpec {
+//!     TimeSpec {
+//!         seconds: 111,
+//!         nanos: 333,
+//!     }
+//! }
+//!
+//! let v = vdso::vDSO::find(None).unwrap();
+//! let og = v.entry(Kind::GetTime).ok_or("Could not find clock").unwrap();
+//! let backup = og.overwrite(myclock);
+//!
 //! // Clock is frozen; all calls to time return the same values
 //! let time_a = SystemTime::now();
 //! let time_b = SystemTime::now();
 //! assert_eq!(time_a, time_b);
 //!
 //! // Restore clock; all calls to time return unique values
-//! ClockController::restore();
+//! backup.restore();
 //! let time_c = SystemTime::now();
 //! let time_d = SystemTime::now();
 //! assert_ne!(time_c, time_d);
 //! ```
 
+mod opcodes;
 pub(crate) mod trampolines;
-pub(crate) mod vdso;
+pub mod vdso;
 
 use libc;
-use std::fs;
 
 use crate::trampolines::*;
 use crate::vdso::vDSO;
@@ -82,113 +84,54 @@ pub type ClockGetResCb = fn(i32) -> TimeSpec;
 pub type ClockGetTimeOfDayCb = fn() -> TimeVal; // FIXME: Needs to take a TZ
 
 #[derive(Clone)]
-pub struct VDSOFun {
+pub struct VDSOFun<'a> {
     pub name: String,
-    //pub kind: Kind,
-    addr: u64,
-    size: u64,
+    addr: usize,
+    size: usize,
+    v: &'a vDSO,
 }
 
-pub struct BackupEntry {
-    v: VDSOFun,
+pub struct BackupEntry<'a> {
+    v: &'a VDSOFun<'a>,
     data: Vec<u8>,
 }
 
-pub struct GTVdso {
-    v: VDSOFun,
+pub struct GTVdso<'a> {
+    v: VDSOFun<'a>,
 }
 
 #[derive(PartialEq)]
-enum Kind {
+pub enum Kind {
     GetTime,
     Time,
     ClockGetRes,
     GetTimeOfDay,
 }
 
-impl BackupEntry {
+impl<'a> BackupEntry<'a> {
     pub fn restore(&self) {
         let r = vDSO::find(None).unwrap();
-        vDSO::restore(r.start as u64, self.v.addr, &self.data)
+        r.overwrite(self.v.addr, &self.data)
     }
 }
 
 pub trait TVDSOFun {
-    fn trampoline(&self) -> u64;
-    fn overwrite(&self, fn_addr: u64) -> BackupEntry;
+    fn overwrite(&self, cb: ClockGetTimeCb) -> BackupEntry;
 }
 
-fn _overwrite(v: &VDSOFun, trampoline: u64) -> BackupEntry {
-    let r = vDSO::find(None).unwrap();
-    unsafe {
-        libc::mprotect(
-            r.start as *mut libc::c_void,
-            r.end - r.start,
-            libc::PROT_EXEC | libc::PROT_WRITE | libc::PROT_READ,
-        );
-    }
-    //let backup = vDSO::read_symbol(r.start as u64, v.addr, v.size as usize);
-    let buf = vDSO::read(&r);
-    let backup = &buf[(v.addr as usize)..(v.addr + v.size) as usize];
-    vDSO::overwrite(r.start as u64, v.addr, trampoline, v.size as usize);
-    // vDSO::overwrite(elf_offset, ds.address, *dst_addr, ds.size as usize);
+fn _overwrite<'a>(v: &'a VDSOFun, trampoline: usize) -> BackupEntry<'a> {
+    let opcodes = opcodes::generate_opcodes(trampoline, v.size);
+    let backup = v.v.symbol_code(&v.name);
+    v.v.overwrite(v.addr, &opcodes);
     BackupEntry {
-        v: v.clone(),
+        v: &v,
         data: backup.to_owned(),
     }
 }
-impl TVDSOFun for GTVdso {
-    fn trampoline(&self) -> u64 {
-        my_clockgettime as *const () as u64
+impl<'a> TVDSOFun for GTVdso<'a> {
+    fn overwrite(&self, cb: ClockGetTimeCb) -> BackupEntry {
+        let mut w = CLOCK_GT_CB.write().unwrap();
+        *w = Some(cb);
+        _overwrite(&self.v, my_clockgettime as *const () as usize)
     }
-    //fn overwrite(&self, cb: ClockGetTimeCb) -> BackupEntry {
-    fn overwrite(&self, fn_addr: u64) -> BackupEntry {
-        //let mut w = CLOCK_GT_CB.write().unwrap();
-        //*w = Some(cb);
-        _overwrite(&self.v, fn_addr)
-    }
-}
-
-pub fn get_time() -> Option<GTVdso> {
-    match entry(Kind::GetTime) {
-        None => None,
-        Some(v) => Some(GTVdso { v }),
-    }
-}
-
-fn entry(wanted: Kind) -> Option<VDSOFun> {
-    let r = vDSO::find(None).unwrap();
-    let buf = vDSO::read(&r);
-    for ds in vDSO::dynsyms(buf) {
-        let kind = match ds.name.as_str() {
-            "clock_gettime" => Some(Kind::GetTime),
-            &_ => None,
-        };
-        if kind.is_none() {
-            continue;
-        }
-        if kind.as_ref() != Some(&wanted) {
-            continue;
-        }
-        let v = VDSOFun {
-            name: ds.name,
-            addr: ds.address,
-            size: ds.size,
-        };
-        match kind {
-            None => {}
-            Some(Kind::GetTime) => return Some(v),
-            Some(_) => {}
-        }
-    }
-    None
-}
-
-#[allow(dead_code)]
-pub fn dump_vdso(suffix: Option<&str>) {
-    println!("Dumping vDSO");
-    let r = vDSO::find(None).unwrap();
-    let cur_vdso = vDSO::read(&r);
-    let fname = format!("/tmp/vdso{}", suffix.unwrap_or(""));
-    fs::write(&fname, cur_vdso).expect(&format!("Unable to write file {}", fname));
 }
