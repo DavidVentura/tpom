c5a9cc545c530cc11d4755b51e5eaa1c16e361f0
DavidVentura <davidventura27@gmail.com>
2024-03-27 09:23:58 +0100

    add multithreaded tests

--- tests/pub.rs
+++ tests/pub.rs
@@ -1,7 +1,9 @@
 mod tests {
     use serial_test::serial;
-    use std::time::SystemTime;
+    use std::time::{SystemTime, Duration};
     use tpom::{vdso, Kind, TVDSOFun, TimeSpec};
+    use std::thread;
+    use std::hint::black_box;
 
     fn myclock(_clockid: i32) -> TimeSpec {
         TimeSpec {
@@ -14,10 +16,11 @@ mod tests {
     #[serial]
     fn regular_clock_produces_different_timestamps() {
         let time_a = SystemTime::now();
-        std::thread::sleep(std::time::Duration::from_millis(1)); // clock in github actions is coarse
+        thread::sleep(std::time::Duration::from_millis(1)); // clock in github actions is coarse
         let time_b = SystemTime::now();
         assert_ne!(time_a, time_b);
     }
+
     #[test]
     #[serial]
     fn it_freezes_system_clock() {
@@ -29,10 +32,34 @@ mod tests {
         let backup = og.overwrite(myclock);
 
         let time_a = SystemTime::now();
-        std::thread::sleep(std::time::Duration::from_millis(1)); // clock in github actions is coarse
+        thread::sleep(std::time::Duration::from_millis(1)); // clock in github actions is coarse
         let time_b = SystemTime::now();
-        backup.restore();
         assert_eq!(time_a, time_b);
+        backup.restore();
+    }
+
+    #[test]
+    #[serial]
+    fn it_works_many_threads() {
+        let v = vdso::vDSO::read().unwrap();
+        let og = v
+            .entry(Kind::GetTime)
+            .ok_or("Could not find clock")
+            .unwrap();
+        let backup = og.overwrite(myclock);
+
+        thread::scope(|s| {
+            for _ in 0..10 {
+                s.spawn(|| {
+                    for _ in 0..100 {
+                        black_box(SystemTime::now());
+                        thread::sleep(Duration::from_millis(1));
+                    }
+                });
+            }
+        });
+        backup.restore();
+        black_box(SystemTime::now());
     }
 
     #[test]
@@ -47,9 +74,9 @@ mod tests {
         let backup = og.overwrite(myclock);
 
         let time_a = SystemTime::now();
-        std::thread::sleep(std::time::Duration::from_millis(1)); // clock in github actions is coarse
+        thread::sleep(std::time::Duration::from_millis(1)); // clock in github actions is coarse
         let time_b = SystemTime::now();
-        backup.restore();
         assert_eq!(time_a, time_b);
+        backup.restore();
     }
 }
