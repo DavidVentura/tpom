f5005d4b8058c92dcd9c6e6fa69f7064b2d0c7f1
David Ventura <davidventura27@gmail.com>
2024-03-28 16:18:25 +0100

    Flush data/instruction cache after modifying vDSO (#4)
    * Flush data/instruction cache after modifying vDSO
    
    * sync tasks via mutex
    
    * guard vdso overwrites with a static mutex

--- src/vdso.rs
+++ src/vdso.rs
@@ -4,6 +4,10 @@ use goblin::strtab::Strtab;
 use core::slice;
 use std::error::Error;
 use std::fs;
+use cacheflush_sys;
+use std::sync::Mutex;
+
+static vdso_mutex: Mutex<i32> = Mutex::new(0);
 
 #[derive(Debug, PartialEq)]
 pub(crate) struct DynSym {
@@ -106,11 +110,20 @@ impl vDSO {
     /// It is the caller's responsibility to provide the correct amount of data.
     pub(crate) fn overwrite(&self, symbol_address: usize, opcodes: &[u8]) {
         let dst_addr = self.avv.vdso_base + symbol_address;
+
+        let _guard = vdso_mutex.lock().unwrap();
         self.change_mode(true);
         unsafe {
             std::ptr::copy_nonoverlapping(opcodes.as_ptr(), dst_addr as *mut u8, opcodes.len())
         };
+        // https://community.arm.com/arm-community-blogs/b/architectures-and-processors-blog/posts/caches-and-self-modifying-code
+        // We need to clear the instruction cache, otherwise it's possible that the old
+        // instructions (the trampoline) get executed with the new data (the original vDSO
+        // function)
         self.change_mode(false);
+        unsafe {
+            cacheflush_sys::flush(dst_addr as *const u8, opcodes.len()).unwrap();
+        }
     }
 
     pub fn entry(&self, wanted: Kind) -> Option<impl TVDSOFun + '_> {
